387cdc6468dea06434b1f146d8ca2ab71578d353
gicxdc <75556620+gicxdc@users.noreply.github.com>
2021-01-03 10:40:55 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let u= document.getElementById('up')
+let i= document.getElementById('in')
+function switch1(){
+    document.getElementById('up').style.display = "none"
+    document.getElementById('in').style.display = "flex"
+}
+function switch2(){
+    document.getElementById('up').style.display = "flex"
+    document.getElementById('in').style.display = "none"
+}
