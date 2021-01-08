eb4a684165c23a8801aa540bd2cda3417883ec67
gicxdc <75556620+gicxdc@users.noreply.github.com>
2021-01-08 23:26:34 +0700

    homepapes

--- script.js
+++ script.js
@@ -1,10 +1,71 @@
-let u= document.getElementById('up')
-let i= document.getElementById('in')
-function switch1(){
-    document.getElementById('up').style.display = "none"
-    document.getElementById('in').style.display = "flex"
+
+function nav6(){
+    
+    document.getElementById('no.6').style.display = "flex"
+    document.getElementById('no.7').style.display = "none"
+    document.getElementById('no.8').style.display = "none"
+    document.getElementById('no.9').style.display = "none"
+    document.getElementById('no.10').style.display = "none"
+    document.getElementById('no.11').style.display = "none"
+    document.getElementById('no.12').style.display = "none"
 }
-function switch2(){
-    document.getElementById('up').style.display = "flex"
-    document.getElementById('in').style.display = "none"
+function nav7(){
+    
+    document.getElementById('no.6').style.display = "none"
+    document.getElementById('no.7').style.display = "flex"
+    document.getElementById('no.8').style.display = "none"
+    document.getElementById('no.9').style.display = "none"
+    document.getElementById('no.10').style.display = "none"
+    document.getElementById('no.11').style.display = "none"
+    document.getElementById('no.12').style.display = "none"
+}
+function nav8(){
+    
+    document.getElementById('no.6').style.display = "none"
+    document.getElementById('no.7').style.display = "none"
+    document.getElementById('no.8').style.display = "flex"
+    document.getElementById('no.9').style.display = "none"
+    document.getElementById('no.10').style.display = "none"
+    document.getElementById('no.11').style.display = "none"
+    document.getElementById('no.12').style.display = "none"
+}
+function nav9(){
+    
+    document.getElementById('no.6').style.display = "none"
+    document.getElementById('no.7').style.display = "none"
+    document.getElementById('no.8').style.display = "none"
+    document.getElementById('no.9').style.display = "flex"
+    document.getElementById('no.10').style.display = "none"
+    document.getElementById('no.11').style.display = "none"
+    document.getElementById('no.12').style.display = "none"
+}
+function nav10(){
+    
+    document.getElementById('no.6').style.display = "none"
+    document.getElementById('no.7').style.display = "none"
+    document.getElementById('no.8').style.display = "none"
+    document.getElementById('no.9').style.display = "none"
+    document.getElementById('no.10').style.display = "flex"
+    document.getElementById('no.11').style.display = "none"
+    document.getElementById('no.12').style.display = "none"
+}
+function nav11(){
+    
+    document.getElementById('no.6').style.display = "none"
+    document.getElementById('no.7').style.display = "none"
+    document.getElementById('no.8').style.display = "none"
+    document.getElementById('no.9').style.display = "none"
+    document.getElementById('no.10').style.display = "none"
+    document.getElementById('no.11').style.display = "flex"
+    document.getElementById('no.12').style.display = "none"
+}
+function nav12(){
+    
+    document.getElementById('no.6').style.display = "none"
+    document.getElementById('no.7').style.display = "none"
+    document.getElementById('no.8').style.display = "none"
+    document.getElementById('no.9').style.display = "none"
+    document.getElementById('no.10').style.display = "none"
+    document.getElementById('no.11').style.display = "none"
+    document.getElementById('no.12').style.display = "flex"
 }
